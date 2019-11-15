547d01d6ccc704554e6af9b52bcd493a9a08d737
martires alberto orozco gonzalez <trans_maog@htomail.com>
2019-11-15 06:25:46 -0600

    terminado el CRUD acercade

--- app/Http/Controllers/AcercadeController.php
+++ app/Http/Controllers/AcercadeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Acerca;
 
 class AcercadeController extends Controller
 {
     public function index()
     {
-        return view('perfumeria.acercade.index');
+
+        $acerca = Acerca::all();
+
+        return view('perfumeria.acercade.index', compact('acerca'));
     }
 }
--- resources/views/perfumeria/acercade/index.blade.php
+++ resources/views/perfumeria/acercade/index.blade.php
@@ -4,56 +4,23 @@
 @section('iniciopagina')
 
 <div class="container" style="margin-top:80px;">
- 
-<div class="intro h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-    <div class="col-md-6">
-      <h1>Quienes Somos</h1>
-      <p>Bienvenido a PerfuVentas somo una empresa dedicada a la venta
-      de perfumes de varias marcas , tenemos varios anos de experiencia
-      en la venta de perfumes , siendo nuestro mayor logro satisfacer las
-      necesidades de nuestros clientes a quienes consideramos tambien nuestros
-      amigos y agradecemos la confianza depositada en nosotrosa</p>
-    </div>
-    <div class="col-md-6 mt-2 mb-4">
-      <img src="/images/acercade.png" class="img-fluid"/>
-    </div>
-  </div>
-</div>
+
+@foreach ($acerca as $acer)
+    
 
 <div class="intro h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-    
-    <div class="col-md-6 mt-2 mb-4">
-      <img src="/images/acercade.png" class="img-fluid"/>
-    </div>
-    <div class="col-md-6">
-      <h1>Acerca de Pefuventas</h1>
-      <p>Bienvenido a PerfuVentas somo una empresa dedicada a la venta
-      de perfumes de varias marcas , tenemos varios anos de experiencia
-      en la venta de perfumes , siendo nuestro mayor logro satisfacer las
-      necesidades de nuestros clientes a quienes consideramos tambien nuestros
-      amigos y agradecemos la confianza depositada en nosotrosa</p>
-    </div>
-    
-  </div>
-</div>
-
-<div class="intro h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-    <div class="col-md-6">
-      <h1>Acerca de Pefuventas</h1>
-      <p>Bienvenido a PerfuVentas somo una empresa dedicada a la venta
-      de perfumes de varias marcas , tenemos varios anos de experiencia
-      en la venta de perfumes , siendo nuestro mayor logro satisfacer las
-      necesidades de nuestros clientes a quienes consideramos tambien nuestros
-      amigos y agradecemos la confianza depositada en nosotrosa</p>
+<div class="row h-100 justify-content-center align-items-center">
+  <div class="col-md-6">
+  <h1>{{ $acer->title}}</h1>
+  <p>{{ $acer->description }}</p>
     </div>
     <div class="col-md-6 mt-2 mb-4">
-      <img src="/images/acercade.png" class="img-fluid"/>
+      <img src="{{ Storage::url($acer->image) }}" class="img-fluid"/>
     </div>
   </div>
 </div>
+  
+@endforeach  
 
 </div>
 
